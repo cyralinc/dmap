ac3ca4cdfa8c36fecd203926eb3dab76b731268b
Victor Moraes <vmoraes@cyral.com>
2024-02-01 20:33:47 -0300

    Add config validation

--- scan/config.go
+++ scan/config.go
@@ -1,11 +1,37 @@
 package scan
 
+import "fmt"
+
 type Config struct {
 	AWS *AWSConfig
 }
 
 type AWSConfig struct {
-	Regions []string
+	Regions    []string
+	AssumeRole *AWSAssumeRoleConfig
 }
 
-// TODO: validate config
+type AWSAssumeRoleConfig struct {
+	IAMRoleARN string
+	ExternalID string
+}
+
+func (c *Config) validateConfig() error {
+	if c.isAWSConfigured() {
+		if len(c.AWS.Regions) == 0 {
+			return fmt.Errorf("AWS regions are required")
+		}
+		for _, region := range c.AWS.Regions {
+			if region == "" {
+				return fmt.Errorf("AWS region can't be empty")
+			}
+		}
+	} else {
+		return fmt.Errorf("AWS configuration must be specified")
+	}
+	return nil
+}
+
+func (c *Config) isAWSConfigured() bool {
+	return c.AWS != nil
+}

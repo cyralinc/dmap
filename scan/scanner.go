087fb2376a16f39c677137a9edf21ed607f4acfe
Victor Moraes <vmoraes@cyral.com>
2024-02-06 15:23:04 -0300

    Move scan type definitions to scanner.go file

--- scan/scanner.go
+++ scan/scanner.go
@@ -2,6 +2,7 @@ package scan
 
 import (
 	"context"
+	"time"
 )
 
 // Scanner is an interface that should be implemented for a specific cloud
@@ -10,4 +11,31 @@ import (
 // environment.
 type Scanner interface {
 	Scan(ctx context.Context) (*ScanResults, error)
+}
+
+// RepoType defines the AWS data repository types supported (e.g. RDS, Redshift,
+// DynamoDB, etc).
+type RepoType string
+
+const (
+	// Repo types
+	RepoTypeRDS      RepoType = "REPO_TYPE_RDS"
+	RepoTypeRedshift RepoType = "REPO_TYPE_REDSHIFT"
+	RepoTypeDynamoDB RepoType = "REPO_TYPE_DYNAMODB"
+)
+
+// Repository represents a scanned data repository.
+type Repository struct {
+	Id         string
+	Name       string
+	Type       RepoType
+	CreatedAt  time.Time
+	Tags       []string
+	Properties any
+}
+
+// ScanResults represents the results of a repository scan, including all the
+// data repositories that were scanned.
+type ScanResults struct {
+	Repositories []Repository
 }

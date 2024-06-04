8afb181a0cf5c46a36f4cc265dfe544ab48611f5
Chris Campo <ccampo133@users.noreply.github.com>
2024-06-04 16:06:07 -0400

    Move API flags to global flags

--- cmd/dmap/main.go
+++ cmd/dmap/main.go
@@ -8,9 +8,12 @@ import (
 )
 
 type Globals struct {
-	LogLevel  logLevelFlag     `help:"Set the logging level (trace|debug|info|warn|error|fatal)" enum:"trace,debug,info,warn,error,fatal" default:"info"`
-	LogFormat logFormatFlag    `help:"Set the logging format (text|json)" enum:"text,json" default:"text"`
-	Version   kong.VersionFlag `name:"version" help:"Print version information and quit"`
+	ClientID     string           `help:"API client ID to access the Dmap API. Optional, but required to publish results to the Dmap service."`
+	ClientSecret string           `help:"API client secret to access the Dmap API. Optional, but required to publish results to the Dmap service."` //#nosec G101 -- false positive
+	ApiBaseUrl   string           `help:"Base URL of the Dmap API." default:"https://api.dmap.cyral.io"`
+	LogLevel     logLevelFlag     `help:"Set the logging level (trace|debug|info|warn|error|fatal)" enum:"trace,debug,info,warn,error,fatal" default:"info"`
+	LogFormat    logFormatFlag    `help:"Set the logging format (text|json)" enum:"text,json" default:"text"`
+	Version      kong.VersionFlag `name:"version" help:"Print version information and quit"`
 }
 
 type logLevelFlag string
@@ -46,16 +49,22 @@ func (l logFormatFlag) AfterApply() error {
 }
 
 type CLI struct {
-	Globals
+	*Globals
 	RepoScan RepoScanCmd `cmd:"" help:"Perform data discovery and classification on a data repository."`
 }
 
-// version is the application version. It is intended to be set at compile time
-// via the linker (e.g. -ldflags="-X main.version=...").
-var version = "dev"
+var (
+	// version is the application version. It is intended to be set at compile
+	// time via the linker (e.g. -ldflags="-X main.version=...").
+	version = "dev"
+	// globals holds the application's global flags. It is a package-level
+	// variable to allow access to the flags from any part of the application,
+	// such as validation functions.
+	globals Globals
+)
 
 func main() {
-	cli := CLI{Globals: Globals{}}
+	cli := CLI{Globals: &globals}
 	ctx := kong.Parse(
 		&cli,
 		kong.Name("dmap"),
@@ -70,6 +79,6 @@ func main() {
 			"version": version,
 		},
 	)
-	err := ctx.Run(&cli.Globals)
+	err := ctx.Run(cli.Globals)
 	ctx.FatalIfErrorf(err)
 }
